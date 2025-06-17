5dcc2c3611de45c1782f36cc58d9e35faecf4830
decimozs <marlonadiguemartin548@gmail.com>
2025-06-17 16:41:22 +0800

    update: change the toast position to top-center

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ export const Route = createRootRoute({
 function RootComponentRoute() {
   return (
     <ThemeProvider>
-      <Toaster />
+      <Toaster position="top-center" />
       <ThemeColorSync />
       <Outlet />
       <TanStackRouterDevtools />
