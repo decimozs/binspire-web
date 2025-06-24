0f97a2b80eb226dbe93748f37c4521cb3f3de162
decimozs <marlonadiguemartin548@gmail.com>
2025-06-24 21:06:52 +0800

    test commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,21 @@
-import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-scan({
-  enabled: false,
-});
-
-// Create a new router instance
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   scrollRestoration: true,
 });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-// Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
