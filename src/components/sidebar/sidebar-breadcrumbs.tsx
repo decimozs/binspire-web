97f8ff577568c70aee1bc4ded6e08fdf582b458c
decimozs <marlonadiguemartin548@gmail.com>
2025-07-12 12:08:48 +0800

    update: base commit

--- src/components/sidebar/sidebar-breadcrumbs.tsx
+++ src/components/sidebar/sidebar-breadcrumbs.tsx
@@ -16,7 +16,7 @@ export default function SidebarBreadcrumbs() {
 
   return (
     <Breadcrumb>
-      <BreadcrumbList>
+      <BreadcrumbList className="text-lg">
         {segments.map((segment, index) => {
           const href = "/" + segments.slice(0, index + 1).join("/");
           const isLast = index === segments.length - 1;
