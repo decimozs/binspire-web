18ad09ce58402be26e1e28342b42c74a1ddb4710
decimozs <marlonadiguemartin548@gmail.com>
2025-07-16 22:47:55 +0800

    update: base commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.em17917c30k"
+    "revision": "0.dj8kruldkeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
